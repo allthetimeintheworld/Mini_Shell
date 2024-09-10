cbefc31a5124e3bb1799ad2ab97d5dc2885c7bec
allthetimeintheworld <thejamesdyar@gmail.com>
2024-09-10 17:35:16 +0200

    builtin fts

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,76 @@
 /*   By: j <j@student.42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 17:02:57 by j                 #+#    #+#             */
-/*   Updated: 2024/09/10 17:02:58 by j                ###   ########.fr       */
+/*   Updated: 2024/09/10 17:28:45 by j                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/mini_shell.h"
+
+int is_builtin(char *cmd)
+{
+	return (!ft_strcmp(cmd, "echo") || !ft_strcmp(cmd, "cd")
+		|| !ft_strcmp(cmd, "pwd") || !ft_strcmp(cmd, "export")
+		|| !ft_strcmp(cmd, "unset") || !ft_strcmp(cmd, "env")
+		|| !ft_strcmp(cmd, "exit"));
+}
+
+void	ft_cd(char **args)
+{
+	if (args[1] == NULL)
+	{
+		if (chdir(getenv("HOME")) != 0)
+			perror("minishell");
+	}
+	else
+	{
+		if (chdir(args[1]) != 0)
+		{
+			ft_putstr_fd("minishell: cd: ", 2);
+			ft_putstr_fd(args[1], 2);
+			ft_putstr_fd(": No such file or directory\n", 2);
+		}
+	}
+}
+
+void	ft_pwd(void)
+{
+	char *cwd;
+
+	cwd = getcwd(NULL, 0);
+	printf("%s\n", cwd);
+	free(cwd);
+}
+
+/*void	ft_cwd(void)
+{
+	char *cwd;
+	
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+		printf("%s\n", cwd);
+	else
+		perror("getcwd() error");
+}*/
+
+void	ft_echo(char **args)
+{
+	int i;
+	int newline;
+
+	newline = 1;
+	i = 1;
+	if (args[1] && !ft_strcmp(args[1], "-n"))
+	{
+		newline = 0;
+		i++;
+	}
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], 1);
+		if (args[i + 1])
+			ft_putstr_fd(" ", 1);
+		i++;
+	}
+	if (newline)
+		printf("\n");
+}	

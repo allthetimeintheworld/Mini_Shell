6a67a8ae3a04d5b047f3ce38d72d26db742200b9
allthetimeintheworld <thejamesdyar@gmail.com>
2024-09-16 14:22:23 +0200

    Check and execute builtins

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: j <j@student.42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 17:02:57 by j                 #+#    #+#             */
-/*   Updated: 2024/09/10 17:28:45 by j                ###   ########.fr       */
+/*   Updated: 2024/09/16 14:20:54 by j                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,33 @@ int is_builtin(char *cmd)
 		|| !ft_strcmp(cmd, "pwd") || !ft_strcmp(cmd, "export")
 		|| !ft_strcmp(cmd, "unset") || !ft_strcmp(cmd, "env")
 		|| !ft_strcmp(cmd, "exit"));
+}
+
+void	execute_builtin(char **args)
+{
+	if (!ft_strcmp(args[0], "cd"))
+		ft_cd(args);
+	else if (!ft_strcmp(args[0], "pwd"))
+		ft_pwd();
+	else if (!ft_strcmp(args[0], "echo"))
+		ft_echo(args);
+	else if (!ft_strcmp(args[0], "exit"))
+		ft_exit(args);
+	else if (!ft_strcmp(args[0], "env"))
+		ft_env();
+	else if (!ft_strcmp(args[0], "export"))
+		ft_export(args);
+	else if (!ft_strcmp(args[0], "unset"))
+		ft_unset(args);
+}
+void	execute_cmd(char **args)
+{
+	if (args[0] == NULL)
+		return ;
+	if (is_builtin(args[0]))
+		execute_builtin(args);
+	else
+		exec_external(args);
 }
 
 void	ft_cd(char **args)
@@ -34,6 +61,7 @@ void	ft_cd(char **args)
 			ft_putstr_fd("minishell: cd: ", 2);
 			ft_putstr_fd(args[1], 2);
 			ft_putstr_fd(": No such file or directory\n", 2);
+			//fd Free?
 		}
 	}
 }

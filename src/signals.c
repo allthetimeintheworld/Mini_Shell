a57173d96c4a422435924735f21c4313bb4d059d
James Dyar <jadyar@c0c7c1.42berlin.de>
2024-09-25 10:50:48 +0200

    sig use with rl lib

--- src/signals.c
+++ src/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: j <j@student.42.fr>                        +#+  +:+       +#+        */
+/*   By: jadyar <jadyar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 18:00:40 by j                 #+#    #+#             */
-/*   Updated: 2024/09/16 14:22:52 by j                ###   ########.fr       */
+/*   Updated: 2024/09/25 10:14:44 by jadyar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 
 void	sigint_handler(int sig)
 {
-	(void)sig;
-	ft_putstr_fd("\n", 1);
+	if (sig == SIGINT || sig == SIGQUIT)
+	{
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
 
 void	setup_signals(void)

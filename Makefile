162d1c338475cd632521ccf2c6b4f10e3f5ce419
djordje82 <127096833+djordje82@users.noreply.github.com>
2024-09-30 15:15:22 +0200

    ft_exit.c function split and norminette

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: jadyar <jadyar@student.42.fr>              +#+  +:+       +#+         #
+#    By: dodordev <dodordev@student.42berlin.de>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/09/05 14:34:50 by j                 #+#    #+#              #
-#    Updated: 2024/09/25 11:00:08 by jadyar           ###   ########.fr        #
+#    Updated: 2024/09/30 15:03:26 by dodordev         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,8 @@ NAME = minishell
 SRCS = src/main.c src/tokenizer.c src/exec.c src/check_find_path.c src/builtin/builtins.c src/utils.c src/parse.c  src/redirections.c src/signals.c src/builtin/ft_echo.c src/builtin/ft_cd.c src/builtin/ft_pwd.c src/builtin/ft_export.c src/builtin/ft_unset.c src/builtin/ft_env.c src/builtin/ft_exit.c
 OBJS = $(SRCS:.c=.o)
 
-CFLAGS = -Wall -Wextra -Werror -g
+CFLAGS = -Wall -Wextra -Werror -g -I/opt/homebrew/opt/readline/include
+LDFLAGS = -L/opt/homebrew/opt/readline/lib
 LIBS = -lreadline
 
 LIBFT_DIR = ./libft
@@ -28,7 +29,7 @@ $(LIBFT):
 	make -C $(LIBFT_DIR)
 
 $(NAME): $(OBJS) $(LIBFT)
-	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) $(LIBS) $(LIBFT)
+	$(CC) $(CFLAGS) $(OBJS) $(LIBFT) $(LDFLAGS) $(LIBS) -o $(NAME)
 
 clean:
 	rm -f $(OBJS)
@@ -39,4 +40,3 @@ fclean: clean
 	make -C $(LIBFT_DIR) fclean
 
 re: fclean all
-
--- src/builtin/ft_exit.c
+++ src/builtin/ft_exit.c
@@ -3,21 +3,96 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jadyar <jadyar@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dodordev <dodordev@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 15:41:57 by jadyar            #+#    #+#             */
-/*   Updated: 2024/09/25 10:57:52 by jadyar           ###   ########.fr       */
+/*   Updated: 2024/09/30 15:14:01 by dodordev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/mini_shell.h"
 #include "../../include/builtins.h"
 
-void	ft_exit(char **args)
+#include "../../include/mini_shell.h"
+#include "../../include/builtins.h"
+
+static int	is_numeric(char *arg)
 {
 	int	i;
 
-	i = 1;
+	i = 0;
+	while (arg[i])
+	{
+		if (!ft_isdigit(arg[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static void	print_numeric_error(char *arg)
+{
+	ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+	ft_putstr_fd(arg, STDERR_FILENO);
+	ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+}
+
+void	ft_exit(char **args)
+{
+	if (args[1])
+	{
+		if (!is_numeric(args[1]))
+		{
+			print_numeric_error(args[1]);
+			exit(255);
+		}
+		if (args[2])
+		{
+			ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+			exit(1);
+		}
+		exit(ft_atoi(args[1]));
+	}
+	else
+		exit(0);
+}
+
+
+/*void	ft_exit(char **args)
+{
+	int	i;
+
+	if (args[1])
+	{
+		i = 0;
+		while (args[1][i])
+		{
+			if (ft_isdigit(args[1][i]) == 0)
+			{
+				ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+				ft_putstr_fd(args[1], STDERR_FILENO);
+				ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+				exit(255);
+			}
+			i++;
+		}
+		if (args[2])
+		{
+			ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+			exit(1);
+		}
+		else
+			exit(ft_atoi(args[1]));
+	}
+	else
+		exit(0);
+}*/
+
+/*void	ft_exit(char **args)
+{
+	//int	i;
+
+	//i = 1;
 	if (args[1])
 	{
 		if (ft_isdigit(args[1][0]) == 0)
@@ -37,4 +112,4 @@ void	ft_exit(char **args)
 	}
 	else
 		exit(0);
-}
+}*/
